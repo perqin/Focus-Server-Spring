21286d7cfac1850676fcfbea034ef7ab0fc434a5
perqin <perqinxie@gmail.com>
2017-08-03 00:45:46 +0800

    Add Spring Security

---
+++ src/main/java/com/perqin/focus/spring/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,11 @@
+package com.perqin.focus.spring.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/**
+ * This class will register the springSecurityFilterChain Filter for every URL in the application. As an implementation
+ * for {@link org.springframework.web.WebApplicationInitializer}, this class will be detected automatically thus the
+ * filter chain will be registered.
+ */
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+}
