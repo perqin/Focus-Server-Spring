427d02e2d0677bb3fff5aabde633b255dd41033f
perqin <perqinxie@gmail.com>
2017-08-17 00:28:13 +0800

    Simplify WebInitializer

--- src/main/java/com/perqin/focus/spring/app/initializers/WebInitializer.java
+++ src/main/java/com/perqin/focus/spring/app/initializers/WebInitializer.java
@@ -1,36 +1,34 @@
 package com.perqin.focus.spring.app.initializers;
 
-import ch.qos.logback.ext.spring.web.LogbackConfigListener;
 import com.perqin.focus.spring.app.config.*;
-import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.ContextLoaderListener;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRegistration;
-
-public class WebInitializer implements WebApplicationInitializer {
+public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
-    public void onStartup(ServletContext servletContext) throws ServletException {
-        // Add Logback config before ContextLoaderListener
-        servletContext.addListener(LogbackConfigListener.class);
-        servletContext.setInitParameter("logbackConfigLocation", "/WEB-INF/logback-spring.xml");
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{
+                SecurityConfig.class,
+                ApiServletConfig.class,
+                PersistenceJpaConfig.class,
+                LogbackConfig.class,
+                ServicesConfig.class
+        };
+    }
 
-        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext();
+    @Override
+    protected String getServletName() {
+        return "api";
+    }
 
-        applicationContext.register(SecurityConfig.class);
-        applicationContext.register(ApiServletConfig.class);
-        applicationContext.register(PersistenceJpaConfig.class);
-        applicationContext.register(LogbackConfig.class);
-        applicationContext.register(ServicesConfig.class);
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{
+                "/api/*"
+        };
+    }
 
-        servletContext.addListener(new ContextLoaderListener(applicationContext));
-
-        AnnotationConfigWebApplicationContext dispatcherContext = new AnnotationConfigWebApplicationContext();
-        ServletRegistration.Dynamic api = servletContext.addServlet("api", new DispatcherServlet(dispatcherContext));
-        api.setLoadOnStartup(1);
-        api.addMapping("/api/*");
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
     }
 }
